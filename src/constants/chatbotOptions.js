b35e8c372e007a908538d431f6f22b08ef238213
Lovish Bir <53646879+lovishbir@users.noreply.github.com>
2025-03-17 15:59:24 +0530

    added constrants

---
+++ src/constants/chatbotOptions.js
@@ -0,0 +1,49 @@
+export const MAIN_OPTIONS = [
+    "Admission Information",
+    "Course Offerings",
+    "Tuition & Fees",
+    "Campus Facilities",
+    "Contact Information"
+  ];
+  
+  export const ADMISSION_OPTIONS = [
+    "Undergraduate Admissions",
+    "Graduate Admissions",
+    "Application Deadlines",
+    "Admission Requirements",
+    "International Students"
+  ];
+  
+  export const COURSE_OPTIONS = [
+    "Arts & Humanities",
+    "Science & Engineering",
+    "Business",
+    "Medicine",
+    "Law",
+    "View All Programs"
+  ];
+  
+  export const FEES_OPTIONS = [
+    "Undergraduate Tuition",
+    "Graduate Tuition",
+    "International Student Fees",
+    "Scholarships",
+    "Payment Plans"
+  ];
+  
+  export const CAMPUS_OPTIONS = [
+    "Libraries",
+    "Housing",
+    "Dining",
+    "Recreation",
+    "Research Facilities",
+    "Campus Map"
+  ];
+  
+  export const CONTACT_OPTIONS = [
+    "Admissions Office",
+    "Financial Aid",
+    "Academic Departments",
+    "Student Services",
+    "IT Support"
+  ];
---
+++ src/constants/intents.js
@@ -0,0 +1,70 @@
+export const INTENTS = {
+    admission: [
+      'admission', 'apply', 'application', 'enroll', 'join', 'how to apply', 
+      'how do i apply', 'application process', 'requirements', 'when can i apply'
+    ],
+    courses: [
+      'course', 'program', 'degree', 'major', 'curriculum', 'study', 'classes',
+      'subjects', 'what can i study', 'what programs', 'what courses', 'offerings'
+    ],
+    fees: [
+      'fee', 'tuition', 'cost', 'payment', 'scholarship', 'financial aid', 'how much',
+      'expensive', 'price', 'pay', 'funding', 'loan', 'grant'
+    ],
+    campus: [
+      'campus', 'location', 'facility', 'building', 'hostel', 'accommodation', 'dorm',
+      'housing', 'residence', 'where is', 'map', 'tour', 'visit'
+    ],
+    contact: [
+      'contact', 'email', 'phone', 'reach', 'talk to', 'speak with', 'call',
+      'number', 'address', 'how to reach', 'connect'
+    ],
+    events: [
+      'event', 'workshop', 'seminar', 'conference', 'webinar', 'lecture',
+      'happening', 'upcoming', 'calendar', 'schedule of events'
+    ],
+    faculty: [
+      'professor', 'teacher', 'faculty', 'staff', 'lecturer', 'instructor',
+      'who teaches', 'department head', 'dean', 'academic staff'
+    ],
+    schedule: [
+      'schedule', 'timetable', 'calendar', 'class timing', 'semester', 'academic year',
+      'when do classes start', 'term dates', 'holidays', 'breaks'
+    ],
+    deadlines: [
+      'deadline', 'due date', 'last date', 'closing date', 'when is the last day',
+      'submission deadline', 'application deadline'
+    ],
+    requirements: [
+      'requirement', 'prerequisite', 'qualification', 'eligible', 'criteria',
+      'what do i need', 'gpa', 'test score', 'documents'
+    ],
+    international: [
+      'international', 'foreign', 'overseas', 'visa', 'i20', 'global',
+      'study abroad', 'exchange program'
+    ],
+    transfer: [
+      'transfer', 'credits', 'prior learning', 'another university', 'switch',
+      'change schools', 'transfer student'
+    ],
+    online: [
+      'online', 'remote', 'distance', 'virtual', 'web-based',
+      'can i study online', 'online degree', 'online courses'
+    ],
+    career: [
+      'career', 'job', 'employment', 'placement', 'internship', 'opportunity',
+      'after graduation', 'alumni', 'prospects'
+    ],
+    greeting: [
+      'hi', 'hello', 'hey', 'greetings', 'good morning', 'good afternoon',
+      'good evening', 'howdy', 'what\'s up', 'hola'
+    ],
+    thanks: [
+      'thank', 'thanks', 'appreciate', 'grateful', 'thank you', 'thanks a lot',
+      'thank you very much', 'appreciate it', 'appreciated'
+    ],
+    goodbye: [
+      'bye', 'goodbye', 'see you', 'talk later', 'farewell', 'have a good day',
+      'have a nice day', 'until next time', 'catch you later'
+    ]
+  };
